52f2a694ddbf2c7ac068b2cbb3203d27fbed5f39
Vahid Porki <ivahidp@gmail.com>
2022-04-23 18:36:55 +0430

    Style hero section for desktop

--- main.js
+++ main.js
@@ -2,11 +2,8 @@ var toggleButton = document.querySelector(".toggle-button");
 var mobileNav = document.querySelector(".mobile-nav");
 var closeButton = document.querySelector(".close-button");
 
-// console.log(mobileNav);
-
 toggleButton.addEventListener("click", function () {
   mobileNav.classList.add("open");
-  toggleButton.style.display = "none";
   setTimeout(function () {
     backdrop.classList.add("open");
   }, 10);
@@ -14,7 +11,6 @@ toggleButton.addEventListener("click", function () {
 
 closeButton.addEventListener("click", function () {
   mobileNav.classList.remove("open");
-  toggleButton.style.display = "block";
   setTimeout(function () {
     backdrop.classList.remove("open");
   }, 10);
